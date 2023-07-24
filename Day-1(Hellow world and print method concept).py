82f6ffbd0e49dc12d1a7e82e86cbc3aba8a0826c
Arjun Singh Shekhawat <121460096+ArjunSinghShekhawat@users.noreply.github.com>
2023-07-24 19:53:24 +0530

    Add files via upload

---
+++ Day-1(Hellow world and print method concept).py
@@ -0,0 +1,15 @@
+#Python First Program
+
+print("code with harry")
+print('hello world')
+print(5)
+print("By")
+print("Marks",55)
+print(17*13)
+print(17/13)
+print(200%2)
+
+print('--porm here--')
+
+
+
